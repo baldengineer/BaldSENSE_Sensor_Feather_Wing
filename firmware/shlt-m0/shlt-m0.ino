56ae1a274746860131633c2bf026787b43cf0f22
James Lewis <byerly0503@gmail.com>
2022-04-21 18:25:50 -0500

    added light sensor to m0 code.

--- firmware/shlt-m0/shlt-m0.ino
+++ firmware/shlt-m0/shlt-m0.ino
@@ -18,14 +18,18 @@
 #include <SPI.h>
 #include <SD.h>
 
-#include "stdlib.h"
+// APDS-9660 Proximity Sensor
+#include <Arduino_APDS9960.h>
 
 ClosedCube_SHT31D sht3xd;
 
+const uint8_t LED_PIN=13;
 
 // Global Stuff
 char time_stamp_string[21];
-char temp_humd_string[16];
+char temp_humd_string[21];
+char file_name[30];
+char color_sensor_string[21];
 File myFile;
 
 // RTC Stuff
@@ -35,6 +39,18 @@ RTC_DS3231 rtc;
 // State Machine stuff
 unsigned long previous_print_millis = 0;
 unsigned long print_interval = 2500;
+int r, g, b, a;
+
+byte init_adps() {
+    Serial.print(("ADPS Init..."));
+    if (!APDS.begin()) {
+      Serial.println("Failed.");
+      while (true); // Stop forever
+      return 0;
+    }
+  Serial.println(F("DONE!"));
+  return 1;
+}
 
 byte init_rtc() {
   if (!rtc.begin()) {
@@ -66,18 +82,24 @@ byte init_sht30() {
 
 byte init_sdcard() {
     Serial.print(F("Initializing SD card..."));
-
     if (!SD.begin(A1)) {
         Serial.println(F("failed!"));
         return 0; // fail!
     }
+
+    DateTime now = rtc.now();
+
+    sprintf(file_name, "test-%02d.txt", now.minute());
+
     Serial.println(F("done."));
-    myFile = SD.open("test.txt", FILE_WRITE);
+    Serial.print(F("Filename: "));
+    Serial.println(file_name);
+    myFile = SD.open(file_name, FILE_WRITE);
 
     // if file opened okay:
     if (myFile) {
         Serial.print(F("Writing Header to SD..."));
-        myFile.println("DATE, TIME, Temperature, Humidity");
+        myFile.println("DATE, TIME, Temperature, Humidity, R, G, B, Ambient");
         myFile.close();
         Serial.println(F("done!"));
     } else {
@@ -86,12 +108,23 @@ byte init_sdcard() {
     return 1;
 }
 
+char *dtostrf (double val, signed char width, unsigned char prec, char *sout) {
+  char fmt[20];
+  sprintf(fmt, "%%%d.%df", width, prec);
+  sprintf(sout, fmt, val);
+  return sout;
+}
+
 void setup () {
     Wire.begin();
     Serial.begin(9600);
-    //while(!Serial);
+    pinMode(LED_PIN, OUTPUT);
+    while(!Serial);
 
     delay(3000); // wait for console opening
+
+    // connect to DS3231 (real-time clock)
+    init_rtc();
 
     // connect to sd card
     init_sdcard();
@@ -99,23 +132,23 @@ void setup () {
     // connect to SHT30 (temp and sensor)
     init_sht30();
 
-    // connect to DS3231 (real-time clock)
-    init_rtc();
-
+    // init color sensor
+    init_adps();
 }
 
 void loop () {
 
     // turn LED off
-    int sleepMS = Watchdog.sleep(1000); 
-    #if defined(USBCON) && !defined(USE_TINYUSB)
-     USBDevice.attach();
-    #endif
+    digitalWrite(LED_PIN, LOW);
+  //  int sleepMS = Watchdog.sleep(1000); 
+//    USBDevice.attach();  // re-attach for M0's USB CDC
+    delay(1000);
     // turn LED on
+    digitalWrite(LED_PIN, HIGH);
 
    // change this code because it doesn't matter now.
     unsigned current_millis = millis();
-    if (current_millis - previous_print_millis >= print_interval) {
+    // if (current_millis - previous_print_millis >= print_interval) {
         get_time_stamp();
         Serial.print(time_stamp_string);
         Serial.print(F(","));
@@ -123,10 +156,24 @@ void loop () {
         get_sht30_data();
         Serial.print(temp_humd_string);
 
-        myFile = SD.open("test.txt", FILE_WRITE);
+        if (APDS.colorAvailable()) {
+          // rrr,ggg,bbb,aaa
+          APDS.readColor(r, g, b, a);
+          sprintf(color_sensor_string, "%03d, %03d, %03d, %03d", r,g,b,a);
+        } else {
+          r = 0;
+          g = 0;
+          b = 0;
+          a = 0;
+        }
+        Serial.print(color_sensor_string);
+
+        myFile = SD.open(file_name, FILE_WRITE);
          myFile.print(time_stamp_string);
          myFile.print(",");
-         myFile.println(temp_humd_string);
+         myFile.print(temp_humd_string);
+         myFile.print(",");
+         myFile.println(color_sensor_string);
         myFile.close();
 
         //sht30 stuff.
@@ -138,7 +185,7 @@ void loop () {
 
         // reset the clock
         previous_print_millis = current_millis;
-    }
+   // }
 
 //    delay(3000);
 }
@@ -156,52 +203,6 @@ void get_time_stamp() {
         now.minute(),
         now.second()
         );
-/*    
-    Serial.print(now.year(), DEC);
-    Serial.print('-');
-    Serial.print(now.month(), DEC);
-    Serial.print('-');
-    Serial.print(now.day(), DEC);
-
-    Serial.print(" (");
-    Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
-    Serial.print(") ");
-    Serial.print(",");
-    Serial.print(now.hour(), DEC);
-    Serial.print(':');
-    Serial.print(now.minute(), DEC);
-    Serial.print(':');
-    Serial.print(now.second(), DEC);
-    Serial.println();
-    */
-
-
-/*    
-    Serial.print(" since midnight 1/1/1970 = ");
-    Serial.print(now.unixtime());
-    Serial.print("s = ");
-    Serial.print(now.unixtime() / 86400L);
-    Serial.println("d");
-
-   
-    // calculate a date which is 7 days and 30 seconds into the future
-    DateTime future (now + TimeSpan(7,12,30,6));
-    
-    Serial.print(" now + 7d + 30s: ");
-    Serial.print(future.year(), DEC);
-    Serial.print('/');
-    Serial.print(future.month(), DEC);
-    Serial.print('/');
-    Serial.print(future.day(), DEC);
-    Serial.print(' ');
-    Serial.print(future.hour(), DEC);
-    Serial.print(':');
-    Serial.print(future.minute(), DEC);
-    Serial.print(':');
-    Serial.print(future.second(), DEC);
-    Serial.println();*/
-    
-  //  Serial.println();
 }
 
 
@@ -209,45 +210,18 @@ void get_time_stamp() {
 ClosedCube SHT31-D [Digital I2C] Humidity and Temperature Sensor Breakout
 MIT License
 */
-
 // print_sht30_result("Pooling Mode", sht3xd.readTempAndHumidity(SHT3XD_REPEATABILITY_HIGH, SHT3XD_MODE_POLLING, 50));
-
 void get_sht30_data() {
     SHT31D result = sht3xd.readTempAndHumidity(SHT3XD_REPEATABILITY_HIGH, SHT3XD_MODE_POLLING, 50);
     if (result.error == SHT3XD_NO_ERROR) {
-        // 100.0C,100.0%
-
-
-       // Serial.print(text);
-      //  Serial.print(",T=");
-//}
-
-/*dtostrf(floatvar, StringLengthIncDecimalPoint, numVarsAfterDecimal, charbuf);
-
-where
-floatvar    float variable
-StringLengthIncDecimalPoint     This is the length of the string that will be created
-numVarsAfterDecimal     The number of digits after the deimal point to print
-charbuf     the array to store the results*/
-
         char temp_str[8];
         dtostrf(result.t, 5, 1, temp_str);
 
         char humd_str[8];
         dtostrf(result.rh, 5, 1, humd_str);
        // sprintf(temp_humd_string, "%fC,%f%", result.t, result,rh);
-        sprintf(temp_humd_string, "%sC,%s%%", temp_str, humd_str);
-
-   /*     Serial.print(",");
-        Serial.print(result.t);
-        //Serial.print("C, RH=");
-        Serial.print("C,");
-        Serial.print(result.rh);
-        Serial.println("%");*/
+        sprintf(temp_humd_string, "%sC,%s%%, ", temp_str, humd_str);
     } else {
-        //Serial.print(text);
-      /*  Serial.print(":[ERROR] Code #");
-        Serial.println(result.error);*/
-        sprintf(temp_humd_string, "ERR:%d", result.error);
+        sprintf(temp_humd_string, "ERR:%d, ", result.error);
     }
 }
