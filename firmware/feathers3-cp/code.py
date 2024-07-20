286a1fa2faeac33250e76fdeb158ab97093fc90c
James Lewis <byerly0503@gmail.com>
2024-07-20 16:19:27 -0500

    battery and usb voltage measurements

--- firmware/feathers3-cp/code.py
+++ firmware/feathers3-cp/code.py
@@ -7,10 +7,12 @@ import feathers3
 import adafruit_sht31d
 from adafruit_apds9960.apds9960 import APDS9960
 from adafruit_apds9960 import colorutility
-## PDM (time and board)
-import array
-import math
-import audiobusio # needed for PDM
+import analogio
+
+## PDM (time and board) - Not implemented
+# import array
+# import math
+# import audiobusio # needed for PDM
 
 print("\nbaldSENSE FeatherS3 A")
 print("\n---------------------")
@@ -66,20 +68,38 @@ def get_light_lux(color_data):
     #print("light lux {}".format(lux))
     return lux
 
-### PDM
+### PDM - not implemented
 # Remove DC bias before computing RMS.
-def mean(values):
-    return sum(values) / len(values)
+# def mean(values):
+#     return sum(values) / len(values)
 
 
-def normalized_rms(values):
-    minbuf = int(mean(values))
-    samples_sum = sum(
-        float(sample - minbuf) * (sample - minbuf)
-        for sample in values
-    )
+# def normalized_rms(values):
+#     minbuf = int(mean(values))
+#     samples_sum = sum(
+#         float(sample - minbuf) * (sample - minbuf)
+#         for sample in values
+#     )
 
-    return math.sqrt(samples_sum / len(values))
+#     return math.sqrt(samples_sum / len(values))
+
+### Battery and USBV Checks
+def get_adc_levels(meas_pin, enable_pin=None):
+    # enable battery voltage divider
+    if (enable_pin is not None):
+        enable_pin.direction = digitalio.Direction.OUTPUT
+        enable_pin.value = True
+
+    adc_levels = meas_pin.value
+
+    # disable battery voltage divider (by going HiZ)
+    if (enable_pin is not None):
+        enable_pin.direction = digitalio.Direction.INPUT
+    return adc_levels
+
+def convert_adc_voltage(adc_levels, div_correction = 2):
+    voltage = ((adc_levels * 2.57) / 51000) * div_correction
+    return voltage
 
 ### Main
 def main():
@@ -92,26 +112,31 @@ def main():
     apds.enable_proximity = True
     get_color_data(apds)
 
-    # PDM
-    # pdm_clock = digitalio.DigitalInOut(board.D11)
-    # pdm_data.direction = digitalio.Direction.OUTPUT
-    # pdm_data = digitalio.DigitalInOut(board.D12)
-    # pdm_data.direction = digitalio.Direction.INPUT
+    # Battery / VUSB
+    meas_batt_en = digitalio.DigitalInOut(board.D16)
+    batt_meas = analogio.AnalogIn(board.A1)
+    vusb_meas = analogio.AnalogIn(board.A0)
 
-    # FYI: PDMIn(clock, data, data)
-    mic = audiobusio.PDMIn(board.D11, board.D12, sample_rate=000, bit_depth=8)
-    mic_samples = array.array('H', * 160)
+    # board.D11 = clock
+    # board.D12 = data
+    # mic = audiobusio.PDMIn(board.D11, board.D12, sample_rate=000, bit_depth=8)
+    # mic_samples = array.array('H', * 160)
+    ## goes into loop:
+    # mic.record(samples, len(samples))
+    # magnitude = normalized_rms(samples)
+    # print((magnitude,))
 
     while True:
         print("\nTemperature : %0.1f C" % get_temperature(sht30))
         print("Humidity    : %0.1f %%" % get_humidity(sht30))
-        print(f"Proximity  : {apds.proximity}")
-        print(f"Color Temp : {get_color_temp(get_color_data(apds))}")
-        print(f"Light Lux  : {get_light_lux(get_color_data(apds))}")
-
-        mic.record(samples, len(samples))
-        magnitude = normalized_rms(samples)
-        print((magnitude,))
+        print(f"Proximity   : {apds.proximity}")
+        print(f"Color Temp  : {get_color_temp(get_color_data(apds))}")
+        print(f"Light Lux   : {get_light_lux(get_color_data(apds))}")
+        batt_level = get_adc_levels(batt_meas, meas_batt_en)
+       #batt_level = get_adc_levels(batt_meas)
+        print(f"Battery Volt: {batt_level}, {convert_adc_voltage(batt_level)}V")
+        VUSB_level = get_adc_levels(vusb_meas)
+        print(f"VUSB Volt   : {VUSB_level}, {convert_adc_voltage(VUSB_level)}V")
 
         time.sleep(2)
 
