fa891bede11a471271efe27b92fe4dc60aa33946
James Lewis <byerly0503@gmail.com>
2024-07-20 17:34:48 -0500

    ds3231 rtc added with support for updating time via serial

--- firmware/feathers3-cp/code.py
+++ firmware/feathers3-cp/code.py
@@ -28,6 +28,9 @@ except Exception as e:
     while(True):
         pass
 
+print("Enable RTC")
+rtc = adafruit_ds3231.DS3231(i2c)
+
 # Modified From todbot's CircuitPython Tricks
 # changed if to while so we get the entire string
 class USBSerialReader:
@@ -103,6 +106,57 @@ def convert_adc_voltage(adc_levels, div_correction = 2):
     voltage = ((adc_levels * 2.57) / 51000) * div_correction
     return voltage
 
+### RTC
+def get_date_time_string(rtc, verbose_date=False):
+    t = rtc.datetime
+    #print(f"rtc datetime: {t}")
+    # days = ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+    date_string = "{:02}-{:02}-{:02}".format(t.tm_year, t.tm_mon, t.tm_mday)
+    time_string = "{:02}:{:02}:{:02}".format(t.tm_hour, t.tm_min, t.tm_sec)
+    if (verbose_date):    
+        print(f"Date        : {date_string}")        
+        print(f"Time        : {time_string}")
+    return (date_string, time_string)
+
+def get_rtc_temperature(rtc):
+    return rtc.temperature
+
+def get_free_memory():
+    return gc.mem_free()
+
+def get_flash_size():
+    flash = os.statvfs('/')
+    flash_size = flash[0] * flash[2]
+    flash_free = flash[0] * flash[3]
+    return (flash_free, flash_size)
+
+def process_time_string(str):
+    global rtc
+    params = str.split(",")
+    elements = len(params)
+    if (elements == 9):
+        # yr = params[0]
+        # yr = yr[1:]
+        # params[0] = yr
+        params[0] = params[0].lstrip('t')
+        params[0] = params[0].lstrip('T')
+        for x in range(9):
+            params[x] = int(params[x])
+        #params[8] = params[8].strip()
+        rtc.datetime = time.struct_time(params)
+    else:
+        print(f"Time String Contains: {elements} fields")
+        print("Time string should contain: tm_year, tm_mon, tm_mday, tm_hour, tm_min, tm_sec, tm_wday, tm_yday, tm_isdst")
+        #T2024,07,20,17,30,30,6,-1,-1
+def handle_serial(usb_serial_in):
+    incoming_string = usb_serial_in.read()  # read until newline, echo back chars
+    #mystr = usb_reader.read(end_char='\t', echo=False) # trigger on tab, no echo
+    if incoming_string:
+        print("got:",incoming_string)
+        # do we need to update rtc?
+        if (incoming_string[0].upper() == 'T'):
+            process_time_string(incoming_string)
+
 ### Main
 def main():
     # Temperature / Humidity
@@ -120,39 +174,36 @@ def main():
     vusb_meas = analogio.AnalogIn(board.A0)
 
     # ds3231 (RTC)
-    rtc = adafruit_ds3231.DS3231(i2c)
     usb_reader = USBSerialReader()
 
-   # days = ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
-
     while True:
+        handle_serial(usb_reader)
+        # sht30
         print("\nTemperature  : %0.1f C" % get_temperature(sht30))
         print("Humidity     : %0.1f %%" % get_humidity(sht30))
 
+        # apds-9660
         print(f"Proximity    : {apds.proximity}")
         print(f"Color Temp   : {get_color_temp(get_color_data(apds))}")
         print(f"Light Lux    : {get_light_lux(get_color_data(apds))}")
-        batt_level = get_adc_levels(batt_meas, meas_batt_en)
-       #batt_level = get_adc_levels(batt_meas)
         
-        print(f"Battery Volt: {batt_level}, {convert_adc_voltage(batt_level)}V")
+        # voltage dividers
+        batt_level = get_adc_levels(batt_meas, meas_batt_en)       
+        print(f"Battery Volt : {batt_level}, {convert_adc_voltage(batt_level)}V")
         VUSB_level = get_adc_levels(vusb_meas)
         print(f"VUSB Volt    : {VUSB_level}, {convert_adc_voltage(VUSB_level)}V")
 
-        t = rtc.datetime
-        #print(f"rtc datetime: {t}")
-        date_string = "{}/{}/{}".format(t.tm_mday, t.tm_mon, t.tm_year)
-        print(f"Date        : {date_string}")
-        time_string = "{}:{:02}:{:02}".format(t.tm_hour, t.tm_min, t.tm_sec)
-        print(f"Time        : {time_string}")
-        print(f"rtc temp    : {rtc.temperature}")
+        # rtc
+        date_string = get_date_time_string(rtc)
+        print(f"Date / Time  : {date_string[0]} {date_string[1]}")
+        print(f"RTC temp     : {get_rtc_temperature(rtc)} C")
 
-        mystr = usb_reader.read()  # read until newline, echo back chars
-        #mystr = usb_reader.read(end_char='\t', echo=False) # trigger on tab, no echo
-        if mystr:
-            print("got:",mystr)
 
-        time.sleep(10)
+        print(f"RAM Free     : {get_free_memory():,}")
+        flash_size = get_flash_size()
+        print(f"Flash Free   : {flash_size[0]:,}")
+        print(f"Flash Size   : {flash_size[1]:,}")
+        time.sleep(2)
 
 if (__name__ == '__main__'):
     main()
