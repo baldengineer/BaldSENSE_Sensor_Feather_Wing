5f4e8990c753ec5b2f1414c1c7e23937e733a01b
James Lewis <byerly0503@gmail.com>
2024-07-20 16:46:33 -0500

    removed pdm references while working on rtc

--- firmware/feathers3-cp/code.py
+++ firmware/feathers3-cp/code.py
@@ -8,11 +8,7 @@ import adafruit_sht31d
 from adafruit_apds9960.apds9960 import APDS9960
 from adafruit_apds9960 import colorutility
 import analogio
-
-## PDM (time and board) - Not implemented
-# import array
-# import math
-# import audiobusio # needed for PDM
+import adafruit_ds3231
 
 print("\nbaldSENSE FeatherS3 A")
 print("\n---------------------")
@@ -31,6 +27,27 @@ except Exception as e:
     print("I2C Failed, is shield connected?")
     while(True):
         pass
+
+# Modified From todbot's CircuitPython Tricks
+# changed if to while so we get the entire string
+class USBSerialReader:
+    """ Read a line from USB Serial (up to end_char), non-blocking, with optional echo """
+    def __init__(self):
+        self.s = ''
+    def read(self,end_char='\n', echo=True):
+        import sys, supervisor
+        n = supervisor.runtime.serial_bytes_available
+        
+        while(n > 0):                # we got bytes!
+            s = sys.stdin.read(n)    # actually read it in
+            if echo: sys.stdout.write(s)  # echo back to human
+            self.s = self.s + s      # keep building the string up
+            if s.endswith(end_char): # got our end_char!
+                rstr = self.s        # save for return
+                self.s = ''          # reset str to beginning
+                return rstr
+            n = supervisor.runtime.serial_bytes_available # update n
+        return None                  # no end_char yet
 
 ### SHT30, 31, and 35 vary in accuracy
 # BaldSENSE has a SHT30 (+/-0.2 from 0 to 65C)
@@ -68,21 +85,6 @@ def get_light_lux(color_data):
     #print("light lux {}".format(lux))
     return lux
 
-### PDM - not implemented
-# Remove DC bias before computing RMS.
-# def mean(values):
-#     return sum(values) / len(values)
-
-
-# def normalized_rms(values):
-#     minbuf = int(mean(values))
-#     samples_sum = sum(
-#         float(sample - minbuf) * (sample - minbuf)
-#         for sample in values
-#     )
-
-#     return math.sqrt(samples_sum / len(values))
-
 ### Battery and USBV Checks
 def get_adc_levels(meas_pin, enable_pin=None):
     # enable battery voltage divider
@@ -117,28 +119,40 @@ def main():
     batt_meas = analogio.AnalogIn(board.A1)
     vusb_meas = analogio.AnalogIn(board.A0)
 
-    # board.D11 = clock
-    # board.D12 = data
-    # mic = audiobusio.PDMIn(board.D11, board.D12, sample_rate=000, bit_depth=8)
-    # mic_samples = array.array('H', * 160)
-    ## goes into loop:
-    # mic.record(samples, len(samples))
-    # magnitude = normalized_rms(samples)
-    # print((magnitude,))
+    # ds3231 (RTC)
+    rtc = adafruit_ds3231.DS3231(i2c)
+    usb_reader = USBSerialReader()
+
+   # days = ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
 
     while True:
-        print("\nTemperature : %0.1f C" % get_temperature(sht30))
-        print("Humidity    : %0.1f %%" % get_humidity(sht30))
-        print(f"Proximity   : {apds.proximity}")
-        print(f"Color Temp  : {get_color_temp(get_color_data(apds))}")
-        print(f"Light Lux   : {get_light_lux(get_color_data(apds))}")
+        print("\nTemperature  : %0.1f C" % get_temperature(sht30))
+        print("Humidity     : %0.1f %%" % get_humidity(sht30))
+
+        print(f"Proximity    : {apds.proximity}")
+        print(f"Color Temp   : {get_color_temp(get_color_data(apds))}")
+        print(f"Light Lux    : {get_light_lux(get_color_data(apds))}")
         batt_level = get_adc_levels(batt_meas, meas_batt_en)
        #batt_level = get_adc_levels(batt_meas)
+        
         print(f"Battery Volt: {batt_level}, {convert_adc_voltage(batt_level)}V")
         VUSB_level = get_adc_levels(vusb_meas)
-        print(f"VUSB Volt   : {VUSB_level}, {convert_adc_voltage(VUSB_level)}V")
+        print(f"VUSB Volt    : {VUSB_level}, {convert_adc_voltage(VUSB_level)}V")
 
-        time.sleep(2)
+        t = rtc.datetime
+        #print(f"rtc datetime: {t}")
+        date_string = "{}/{}/{}".format(t.tm_mday, t.tm_mon, t.tm_year)
+        print(f"Date        : {date_string}")
+        time_string = "{}:{:02}:{:02}".format(t.tm_hour, t.tm_min, t.tm_sec)
+        print(f"Time        : {time_string}")
+        print(f"rtc temp    : {rtc.temperature}")
+
+        mystr = usb_reader.read()  # read until newline, echo back chars
+        #mystr = usb_reader.read(end_char='\t', echo=False) # trigger on tab, no echo
+        if mystr:
+            print("got:",mystr)
+
+        time.sleep(10)
 
 if (__name__ == '__main__'):
     main()
